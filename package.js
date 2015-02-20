241e669a3a53c4a719cafdd93451eb8d425d7353
Pusher Robot <jack@semantic-ui.com>
2015-02-20 17:15:49 -0500

    Updated component to version 1.9.3

--- package.js
+++ package.js
@@ -1,17 +1,14 @@
-var
-  where = 'client' // Adds files only to the client
-;
 
 Package.describe({
   name    : 'semantic:ui-input',
-  summary : 'Semantic UI - Input (official): Single component release of input',
-  version : '1.9.2',
+  summary : 'Semantic UI - Input: Single component release',
+  version : '1.9.3',
   git     : 'git://github.com/Semantic-Org/UI-Input.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    
-  ], where);
+    'input.css'
+  ], 'client');
 });
