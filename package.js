ff8e78c7b432788c0bd71084eba04342f29f6f00
Pusher Robot <jack@semantic-ui.com>
2015-02-24 12:10:26 -0500

    Updated component to version 1.10.1

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-input',
   summary : 'Semantic UI - Input: Single component release',
-  version : '1.10.0',
+  version : '1.10.1',
   git     : 'git://github.com/Semantic-Org/UI-Input.git',
 });
 
